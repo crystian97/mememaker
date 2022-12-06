7c8a8cbf3c5255ea35c07c06811a651c840a4cc6
crystian97 <icrystianveloso@gmail.com>
2022-12-06 17:06:20 -0300

    feat:call api and logic to selected template

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Form, Templates, Wrapper } from "./styles";
 import Logo from "../../images/logo.svg";
 export default function Home() {
+    const [templates,setTemplates] = useState([])
+    const [selectedTemplate,setSelectedTemplate] = useState(null)
+    useEffect(()=>{
+        (async ()=>{
+            const resp = await fetch('https://api.imgflip.com/get_memes');
+            const {data:{memes}} = await resp.json();
+            setTemplates(memes)
+        })()
+    })
   return (
     <Wrapper>
       <img src={Logo} />
       <Card>
         <h2>Selecione um template</h2>
         <Templates>
-          <button>
-            <img></img>
+          {templates.map((template)=>(
+            <button
+            key={template.id}
+            onClick={()=>setSelectedTemplate(template)}
+            className={template.id === selectedTemplate?.id && 'selected'}
+            >
+            <img src={template.url} alt={template.name}></img>
           </button>
-          <button>
-            <img></img>
-          </button>
-          <button>
-            <img></img>
-          </button>
-          <button>
-            <img></img>
-          </button>
-          <button>
-            <img></img>
-          </button>
+          ))}
+          
         </Templates>
         <h2>Textos</h2>
         <Form>
@@ -30,7 +34,7 @@ export default function Home() {
             <input placeholder="Texto"/>
             <input placeholder="Texto"/>
             <input placeholder="Texto"/>
-            <Button type="submit">Make my meme</Button>
+            <Button type="submit">MakeMyMeme</Button>
         </Form>
       </Card>
     </Wrapper>
