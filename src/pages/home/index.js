85dce6a91c182a287057c6a673aea7b5ecef27c1
crystian97 <icrystianveloso@gmail.com>
2022-12-06 16:37:09 -0300

    feat:templates

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Card, Templates, Wrapper } from "./styles";
-import Logo from '../../images/logo.svg'
-export default function Home(){
-    return (
-        <Wrapper>
-            <img src={Logo}/>
-            <Card>
-                <h2>Selecione um template</h2>
-                <Templates>
-                    
-                </Templates>
-            </Card>
-        </Wrapper>
-    )
+import Logo from "../../images/logo.svg";
+export default function Home() {
+  return (
+    <Wrapper>
+      <img src={Logo} />
+      <Card>
+        <h2>Selecione um template</h2>
+        <Templates>
+          <button>
+            <img></img>
+          </button>
+          <button>
+            <img></img>
+          </button>
+          <button>
+            <img></img>
+          </button>
+          <button>
+            <img></img>
+          </button>
+          <button>
+            <img></img>
+          </button>
+        </Templates>
+      </Card>
+    </Wrapper>
+  );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,30 +1,43 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 export const Card = styled.div`
-    background:#fff;
-    width: 550px;
-    border-radius: 8px;
-    padding:20px;
-    margin-top: 15px;
-    box-shadow: 0 6px 10px 0 rgba(0,0,0,0.20);
-    h2{
-        font-size:22px;
-        color: #392d2d;
-        margin-bottom: 10px;
-    }
-`
+  background: #fff;
+  width: 550px;
+  border-radius: 8px;
+  padding: 20px;
+  margin-top: 15px;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+  h2 {
+    font-size: 22px;
+    color: #392d2d;
+    margin-bottom: 10px;
+  }
+`;
 export const Templates = styled.div`
-width: 100%;
-height: 90px;
-background: #eee;
-border-radius: 8px;
-overflow-y: auto;
-`
+  width: 100%;
+  height: 90px;
+  background: #eee;
+  border-radius: 8px;
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  padding:0 15px;
+  margin-bottom: 30px;
+  button{
+    border:0;
+    background: transparent;
+    margin-right: 10px;
+    img{
+        width:53px;
+        height:53px;
+    }
+  }
+`;
